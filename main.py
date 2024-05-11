47cb518779368d895004f438ebaa3c4ccfc30f29
Ívyna Rufino <ivynalavinia@gmail.com>
2024-05-10 22:07:00 -0300

    Adjusts execution time calculation by request in both algorithms and makes related changes to study cases using it

--- main.py
+++ main.py
@@ -20,45 +20,49 @@ def primeiro_caso(should_print_and_plot):
     requests_ord = [278, 914, 447, 71, 161, 659, 335]
     requests_aleat = [71, 161, 278, 335, 447, 659, 914]
 
-    cscan = Cscan(requests_ord, 500)
-    seek_sequence1, seek_count1, execution_time1 = cscan.execute()
+    cscan_ord = Cscan(requests_ord, 500)
+    seek_sequence_cscan_ord, seek_count_cscan_ord, execution_time_cscan_ord = cscan_ord.execute()
+    media_tempo_execucao_cscan_ord = statistics.mean(execution_time_cscan_ord)
 
-    sstf = Sstf(requests_ord, 500)
-    seek_sequence2, seek_count2, execution_time2 = sstf.execute()
+    sstf_ord = Sstf(requests_ord, 500)
+    seek_sequence_sstf_ord, seek_count_sstf_ord, execution_time_sstf_ord = sstf_ord.execute()
+    media_tempo_execucao_sstf_ord = statistics.mean(execution_time_sstf_ord)
 
-    cscan2 = Cscan(requests_aleat, 500)
-    seek_sequence3, seek_count3, execution_time3 = cscan2.execute()
+    cscan_aleat = Cscan(requests_aleat, 500)
+    seek_sequence_cscan_aleat, seek_count_cscan_aleat, execution_time_cscan_aleat = cscan_aleat.execute()
+    media_tempo_execucao_cscan_aleat = statistics.mean(execution_time_cscan_aleat)
 
-    sstf2 = Sstf(requests_aleat, 500)
-    seek_sequence4, seek_count4, execution_time4 = sstf2.execute()
+    sstf_aleat = Sstf(requests_aleat, 500)
+    seek_sequence_sstf_aleat, seek_count_sstf_aleat, execution_time_sstf_aleat = sstf_aleat.execute()
+    media_tempo_execucao_sstf_aleat = statistics.mean(execution_time_sstf_aleat)
 
     if should_print_and_plot:
         print("C-SCAN lista ordenada:")
-        print(f"Contagem de seeks: {seek_count1}")
-        print(f"Tempo de execução: {execution_time1}\n")
+        print(f"Contagem de seeks: {seek_count_cscan_ord}")
+        print(f"Média tempo de execução: {media_tempo_execucao_cscan_ord}\n")
 
         print("SSTF lista ordenada:")
-        print(f"Contagem de seeks: {seek_count2}")
-        print(f"Tempo de execução: {execution_time2}\n")
+        print(f"Contagem de seeks: {seek_count_sstf_ord}")
+        print(f"Média tempo de execução: {media_tempo_execucao_sstf_ord}\n")
 
         print("C-SCAN lista aleatória:")
-        print(f"Contagem de seeks: {seek_count3}")
-        print(f"Tempo de execução: {execution_time3}\n")
+        print(f"Contagem de seeks: {seek_count_cscan_aleat}")
+        print(f"Média tempo de execução: {media_tempo_execucao_cscan_aleat}\n")
 
         print("SSTF lista aleatória:")
-        print(f"Contagem de seeks: {seek_count4}")
-        print(f"Tempo de execução: {execution_time4}\n")
+        print(f"Contagem de seeks: {seek_count_sstf_aleat}")
+        print(f"Média tempo de execução: {media_tempo_execucao_sstf_aleat}\n")
 
         plt.figure(figsize=(10, 5))
-        plt.plot(seek_sequence3, marker='o', color='purple')
+        plt.plot(seek_sequence_cscan_aleat, marker='o', color='purple')
         plt.title('Comportamento Circular do Algoritmo C-SCAN')
         plt.xlabel('Sequência de Requisições')
         plt.ylabel('Posição no Disco')
-        plt.xticks(range(len(seek_sequence3)), labels=seek_sequence3)
+        plt.xticks(range(len(seek_sequence_cscan_aleat)), labels=seek_sequence_cscan_aleat)
         plt.grid(True)
         plt.show()
 
-    return seek_count1, execution_time1, seek_count2, execution_time2
+    return seek_count_cscan_ord, media_tempo_execucao_cscan_ord, seek_count_sstf_ord, media_tempo_execucao_sstf_ord, seek_count_cscan_aleat, media_tempo_execucao_cscan_aleat, seek_count_sstf_aleat, media_tempo_execucao_sstf_aleat
 
 
 def plota_grafico_comparacao_seek_count(seek_count_cscan, seek_count_sstf, xlabel, ylabel, title):
@@ -80,17 +84,18 @@ def plota_grafico_comparacao_seek_count(seek_count_cscan, seek_count_sstf, xlabe
     plt.show()
 
 
-def plota_grafico_comparacao_execution_time(execution_time_cscan, execution_time_sstf, xlabel, ylabel, title):
+def plota_grafico_comparacao_execution_time(media_execution_time_cscan, media_execution_time_sstf, xlabel, ylabel,
+                                            title):
     algorithms = ['C-SCAN', 'SSTF']
-    seek_counts = [execution_time_cscan, execution_time_sstf]
+    execution_times_avg = [media_execution_time_cscan, media_execution_time_sstf]
 
     plt.figure(figsize=(10, 5))
-    bars = plt.barh(algorithms, seek_counts, color=['purple', 'green'])
+    bars = plt.barh(algorithms, execution_times_avg, color=['purple', 'green'])
 
-    for bar, value in zip(bars, seek_counts):
+    for bar, value in zip(bars, execution_times_avg):
         bbox = dict(boxstyle='round', facecolor='white', edgecolor='0.3')
         plt.text(bar.get_width(), bar.get_y() + bar.get_height() / 2, f'{value}',
-                 va='center', ha='right', bbox=bbox)
+                 va='center', ha='left', bbox=bbox)
 
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -102,34 +107,72 @@ def plota_grafico_comparacao_execution_time(execution_time_cscan, execution_time
 
 
 def segundo_caso():
-    requests = [random.randint(0, 999) for _ in range(10000)]
-    requests.sort()
+    requests_aleat = [random.randint(0, 999) for _ in range(10000)]
+    requests_ord = requests_aleat.copy()
+    requests_ord.sort()
 
-    seek_count_pq_cscan, execution_time_pq_cscan, seek_count_pq_sstf, execution_time_pq_sstf = primeiro_caso(False)
+    seek_count_pq_cscan_ord, media_tempo_execucao_pq_cscan_ord, seek_count_pq_sstf_ord, media_tempo_execucao_pq_sstf_ord, seek_count_pq_cscan_aleat, media_tempo_execucao_pq_cscan_aleat, seek_count_pq_sstf_aleat, media_tempo_execucao_pq_sstf_aleat = primeiro_caso(
+        False)
 
-    cscan = Cscan(requests, 500)
-    seek_sequence_gd_cscan, seek_count_gd_cscan, execution_time_gd_cscan = cscan.execute()
+    cscan_ord = Cscan(requests_ord, 500)
+    _, seek_count_gd_cscan_ord, execution_time_gd_cscan_ord = cscan_ord.execute()
+    media_tempo_execucao_gd_cscan_ord = statistics.mean(execution_time_gd_cscan_ord)
     print("C-SCAN lista ordenada 10 mil elementos:")
-    print(f"Contagem de seeks: {seek_count_gd_cscan}")
-    print(f"Tempo de execução: {execution_time_gd_cscan}\n")
+    print(f"Contagem de seeks: {seek_count_gd_cscan_ord}")
+    print(f"Média tempo de execução: {media_tempo_execucao_gd_cscan_ord}\n")
 
-    sstf = Sstf(requests, 500)
-    seek_sequence_gd_sstf, seek_count_gd_sstf, execution_time_gd_sstf = sstf.execute()
+    sstf_ord = Sstf(requests_ord, 500)
+    _, seek_count_gd_sstf_ord, execution_time_gd_sstf_ord = sstf_ord.execute()
+    media_tempo_execucao_gd_sstf_ord = statistics.mean(execution_time_gd_sstf_ord)
     print("SSTF lista ordenada 10 mil elementos:")
-    print(f"Contagem de seeks: {seek_count_gd_sstf}")
-    print(f"Tempo de execução: {execution_time_gd_sstf}\n")
+    print(f"Contagem de seeks: {seek_count_gd_sstf_ord}")
+    print(f"Média tempo de execução: {media_tempo_execucao_gd_sstf_ord}\n")
 
-    plota_grafico_comparacao_seek_count(seek_count_pq_cscan, seek_count_pq_sstf, 'Quantidade de Seeks', 'Algoritmo',
+    plota_grafico_comparacao_seek_count(seek_count_pq_cscan_ord, seek_count_pq_sstf_ord, 'Quantidade de Seeks',
+                                        'Algoritmo',
                                         'Comparação da Contagem de Seeks entre C-SCAN e SSTF para uma lista pequena e ordenada')
-    plota_grafico_comparacao_execution_time(execution_time_pq_cscan, execution_time_pq_sstf, 'Tempo de execução (ms)',
+    plota_grafico_comparacao_execution_time(media_tempo_execucao_pq_cscan_ord, media_tempo_execucao_pq_sstf_ord,
+                                            'Tempo de execução (ms)',
                                             'Algoritmo',
-                                            'Comparação do tempo de execução entre C-SCAN e SSTF para uma lista pequena e ordenada')
+                                            'Comparação da média do tempo de execução entre C-SCAN e SSTF para uma lista pequena e ordenada')
 
-    plota_grafico_comparacao_seek_count(seek_count_gd_cscan, seek_count_gd_sstf, 'Quantidade de Seeks', 'Algoritmo',
+    plota_grafico_comparacao_seek_count(seek_count_gd_cscan_ord, seek_count_gd_sstf_ord, 'Quantidade de Seeks',
+                                        'Algoritmo',
                                         'Comparação da Contagem de Seeks entre C-SCAN e SSTF para uma lista com 10 mil elementos e ordenada')
-    plota_grafico_comparacao_execution_time(execution_time_gd_cscan, execution_time_gd_sstf, 'Tempo de execução (ms)',
+    plota_grafico_comparacao_execution_time(media_tempo_execucao_gd_cscan_ord, media_tempo_execucao_gd_sstf_ord,
+                                            'Tempo de execução (ms)',
                                             'Algoritmo',
-                                            'Comparação do tempo de execução entre C-SCAN e SSTF para uma lista com 10 mil elementos e ordenada')
+                                            'Comparação da média do tempo de execução entre C-SCAN e SSTF para uma lista com 10 mil elementos e ordenada')
+
+    cscan_aleat = Cscan(requests_aleat, 500)
+    _, seek_count_gd_cscan_aleat, execution_time_gd_cscan_aleat = cscan_aleat.execute()
+    media_tempo_gd_execucao_cscan_aleat = statistics.mean(execution_time_gd_cscan_aleat)
+    print("C-SCAN lista ordenada 10 mil elementos:")
+    print(f"Contagem de seeks: {seek_count_gd_cscan_aleat}")
+    print(f"Média tempo de execução: {media_tempo_gd_execucao_cscan_aleat}\n")
+
+    sstf_aleat = Sstf(requests_aleat, 500)
+    _, seek_count_gd_sstf_aleat, execution_time_gd_sstf_aleat = sstf_aleat.execute()
+    media_tempo_gd_execucao_sstf_aleat = statistics.mean(execution_time_gd_sstf_aleat)
+    print("SSTF lista ordenada 10 mil elementos:")
+    print(f"Contagem de seeks: {seek_count_gd_sstf_aleat}")
+    print(f"Média tempo de execução: {media_tempo_gd_execucao_sstf_aleat}\n")
+
+    plota_grafico_comparacao_seek_count(seek_count_pq_cscan_aleat, seek_count_pq_sstf_aleat, 'Quantidade de Seeks',
+                                        'Algoritmo',
+                                        'Comparação da Contagem de Seeks entre C-SCAN e SSTF para uma lista pequena e aleatória')
+    plota_grafico_comparacao_execution_time(media_tempo_execucao_pq_cscan_aleat, media_tempo_execucao_pq_sstf_aleat,
+                                            'Tempo de execução (ms)',
+                                            'Algoritmo',
+                                            'Comparação da média do tempo de execução entre C-SCAN e SSTF para uma lista pequena e aleatória')
+
+    plota_grafico_comparacao_seek_count(seek_count_gd_cscan_aleat, seek_count_gd_sstf_aleat, 'Quantidade de Seeks',
+                                        'Algoritmo',
+                                        'Comparação da Contagem de Seeks entre C-SCAN e SSTF para uma lista com 10 mil elementos e aleatória')
+    plota_grafico_comparacao_execution_time(media_tempo_gd_execucao_cscan_aleat, media_tempo_gd_execucao_sstf_aleat,
+                                            'Tempo de execução (ms)',
+                                            'Algoritmo',
+                                            'Comparação da média do tempo de execução entre C-SCAN e SSTF para uma lista com 10 mil elementos e aleatória')
 
 
 def histograma_medias(seek_counts, title, ylabel, order):
@@ -173,12 +216,12 @@ def terceiro_caso():
         cscan = Cscan(lista, 500)
         _, seek_count_cscan, execution_time_cscan = cscan.execute()
         lista_seek_count_cscan_ALEATORIA.append(seek_count_cscan)
-        lista_execution_time_cscan_ALEATORIA.append(execution_time_cscan)
+        lista_execution_time_cscan_ALEATORIA.append(statistics.mean(execution_time_cscan))
 
         sstf = Sstf(lista, 500)
         _, seek_count_sstf, execution_time_sstf = sstf.execute()
         lista_seek_count_sstf_ALEATORIA.append(seek_count_sstf)
-        lista_execution_time_sstf_ALEATORIA.append(execution_time_sstf)
+        lista_execution_time_sstf_ALEATORIA.append(statistics.mean(execution_time_sstf))
 
     print("LISTAS ALEATÓRIAS")
     print("C-SCAN:")
@@ -202,31 +245,39 @@ def terceiro_caso():
         cscan = Cscan(lista, 500)
         _, seek_count_cscan, execution_time_cscan = cscan.execute()
         lista_seek_count_cscan_SEQUENCIAL.append(seek_count_cscan)
-        lista_execution_time_cscan_SEQUENCIAL.append(execution_time_cscan)
+        lista_execution_time_cscan_SEQUENCIAL.append(statistics.mean(execution_time_cscan))
 
         sstf = Sstf(lista, 500)
         _, seek_count_sstf, execution_time_sstf = sstf.execute()
         lista_seek_count_sstf_SEQUENCIAL.append(seek_count_sstf)
-        lista_execution_time_sstf_SEQUENCIAL.append(execution_time_sstf)
+        lista_execution_time_sstf_SEQUENCIAL.append(statistics.mean(execution_time_sstf))
 
-    print("LISTAS SEQUENCIAIS")
-    print("CSCAN")
+    print("\nLISTAS SEQUENCIAIS")
+    print("C-SCAN:")
     seek_media_cscan_sequencial = statistics.mean(lista_seek_count_cscan_SEQUENCIAL)
     execution_medio_cscan_sequencial = statistics.mean(lista_execution_time_cscan_SEQUENCIAL)
     print(f"Quantidade de seek média: {seek_media_cscan_sequencial}")
     print(f"Tempo de execução médio: {execution_medio_cscan_sequencial}")
 
-    print("SSTF")
+    print("SSTF:")
     seek_media_sstf_sequencial = statistics.mean(lista_seek_count_sstf_SEQUENCIAL)
     execution_medio_sstf_sequencial = statistics.mean(lista_execution_time_sstf_SEQUENCIAL)
     print(f"Quantidade de seek média: {seek_media_sstf_sequencial}")
     print(f"Tempo de execução médio: {execution_medio_sstf_sequencial}")
 
     histograma_medias(
-        [statistics.mean(lista_seek_count_cscan_ALEATORIA), statistics.mean(lista_seek_count_cscan_SEQUENCIAL),
-         statistics.mean(lista_seek_count_sstf_ALEATORIA), statistics.mean(lista_seek_count_sstf_SEQUENCIAL)],
+        [seek_media_cscan_aleatoria, seek_media_cscan_sequencial, seek_media_sstf_aleatoria,
+         seek_media_sstf_sequencial],
         'Comparação de Quantidade de Seeks por Algoritmo e Tipo de Lista',
         'Média de Quantidade de Seeks',
+        ['Aleatórias', 'Sequenciais']
+    )
+
+    histograma_medias(
+        [execution_medio_cscan_aleatoria, execution_medio_cscan_sequencial, execution_medio_sstf_aleatoria,
+         execution_medio_sstf_sequencial],
+        'Comparação de Tempo de Execução por Algoritmo e Tipo de Lista',
+        'Média de Tempo de Execução (ms)',
         ['Aleatórias', 'Sequenciais']
     )
 
